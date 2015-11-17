3b895632489f6b0b93b4ed479baef33a8a546360
andre-yai <yai.andre@gmail.com>
2015-11-17 19:17:39 -0200

    NodeJS

---
+++ comidaki-node/comidaki/models/database.js
@@ -0,0 +1,19 @@
+var pg = require('pg');
+var fs = require('fs');
+
+var sql = fs.readFileSync('modelofisicoREDUZIDO.sql').toString();
+
+var conString = 'postgres://andrey:z9lucky@localhost:5432/todo';
+var client = new pg.Client(conString);
+client.connect(function(err, client) {
+  if(err) {
+    return console.error('could not connect to postgres', err);
+  }
+  client.query(sql, function(err, result){
+        if(err){
+            console.log('error: ', err);
+            process.exit(1);
+        }
+        process.exit(0);
+    });
+});
